d1d3fe43a052e0443c162e899473bf74480c0487
kushwahachandanbtry <kushwahachandan44@gmail.com>
2025-08-06 17:37:06 +0545

    Fix error

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -1,140 +1,140 @@
-"use client"
+// "use client"
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-
-
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
-
-import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-
-import { Input } from "@/components/ui/input"
-import { aspectRatioOptions, creditFee, defaultValues, transformationTypes } from "@/constants"
-import { CustomField } from "./CustomField"
-import { useEffect, useState, useTransition } from "react"
-import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils"
-import MediaUploader from "./MediaUploader"
-import TransformedImage from "./TransformedImage"
-import { updateCredits } from "@/lib/actions/user.actions"
-import { getCldImageUrl } from "next-cloudinary"
-import { addImage, updateImage } from "@/lib/actions/image.actions"
-import { useRouter } from "next/navigation"
-import { InsufficientCreditsModal } from "./InsufficientCreditsModal"
-
-export const formSchema = z.object({
-  title: z.string(),
-  aspectRatio: z.string().optional(),
-  color: z.string().optional(),
-  prompt: z.string().optional(),
-  publicId: z.string(),
-})
+// import { zodResolver } from "@hookform/resolvers/zod"
+// import { useForm } from "react-hook-form"
+// import { z } from "zod"
 
 
-const TransformationForm = ({ action, data = null, userId, type, creditBalance, config = null }: TransformationFormProps) => {
+// import {
+//   Select,
+//   SelectContent,
+//   SelectItem,
+//   SelectTrigger,
+//   SelectValue,
+// } from "@/components/ui/select"
 
-  const initialValues = data && action === 'Update' ? {
-    title: data?.title,
-    aspectRatio: data?.aspectRatio,
-    color: data?.color,
-    prompt: data?.prompt,
-    publicId: data?.publicId,
-  } : defaultValues
+// import { Button } from "@/components/ui/button"
+// import {
+//   Form,
+//   FormControl,
+//   FormDescription,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from "@/components/ui/form"
 
-  // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: initialValues,
-  })
+// import { Input } from "@/components/ui/input"
+// import { aspectRatioOptions, creditFee, defaultValues, transformationTypes } from "@/constants"
+// import { CustomField } from "./CustomField"
+// import { useEffect, useState, useTransition } from "react"
+// import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils"
+// import MediaUploader from "./MediaUploader"
+// import TransformedImage from "./TransformedImage"
+// import { updateCredits } from "@/lib/actions/user.actions"
+// import { getCldImageUrl } from "next-cloudinary"
+// import { addImage, updateImage } from "@/lib/actions/image.actions"
+// import { useRouter } from "next/navigation"
+// import { InsufficientCreditsModal } from "./InsufficientCreditsModal"
 
-  // 2. Define a submit handler.
-  async function onSubmit(values: z.infer<typeof formSchema>) {
-    setIsSubmitting(true);
-
-    if (data || image) {
-      const transformationUrl = getCldImageUrl({
-        width: image?.width,
-        height: image?.height,
-        src: image?.publicId,
-        ...transformationConfig
-      })
-
-      const imageData = {
-        title: values.title,
-        publicId: image?.publicId,
-        transformationType: type,
-        width: image?.width,
-        height: image?.height,
-        config: transformationConfig,
-        secureURL: image?.secureURL,
-        transformationURL: transformationUrl,
-        aspectRatio: values.aspectRatio,
-        prompt: values.prompt,
-        color: values.color,
-      }
-
-      if (action === 'Add') {
-        try {
-          const newImage = await addImage({
-            image: imageData,
-            userId,
-            path: '/'
-          })
-
-          if (newImage) {
-            form.reset()
-            setImage(data)
-            router.push(`/transformations/${newImage._id}`)
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      }
-
-      if (action === 'Update') {
-        try {
-          const updatedImage = await updateImage({
-            image: {
-              ...imageData,
-              _id: data._id
-            },
-            userId,
-            path: `/transformations/${data._id}`
-          })
-
-          if (updatedImage) {
-            router.push(`/transformations/${updatedImage._id}`)
-          }
-        } catch (error) {
-          console.log(error);
-        }
-      }
-    }
-
-    setIsSubmitting(false)
-  }
+// export const formSchema = z.object({
+//   title: z.string(),
+//   aspectRatio: z.string().optional(),
+//   color: z.string().optional(),
+//   prompt: z.string().optional(),
+//   publicId: z.string(),
+// })
 
 
-  return (
-    <div>
-      TransformationForm
-    </div>
-  )
-}
+// const TransformationForm = ({ action, data = null, userId, type, creditBalance, config = null }: TransformationFormProps) => {
 
-export default TransformationForm
+//   const initialValues = data && action === 'Update' ? {
+//     title: data?.title,
+//     aspectRatio: data?.aspectRatio,
+//     color: data?.color,
+//     prompt: data?.prompt,
+//     publicId: data?.publicId,
+//   } : defaultValues
+
+//   // 1. Define your form.
+//   const form = useForm<z.infer<typeof formSchema>>({
+//     resolver: zodResolver(formSchema),
+//     defaultValues: initialValues,
+//   })
+
+//   // 2. Define a submit handler.
+//   async function onSubmit(values: z.infer<typeof formSchema>) {
+//     setIsSubmitting(true);
+
+//     if (data || image) {
+//       const transformationUrl = getCldImageUrl({
+//         width: image?.width,
+//         height: image?.height,
+//         src: image?.publicId,
+//         ...transformationConfig
+//       })
+
+//       const imageData = {
+//         title: values.title,
+//         publicId: image?.publicId,
+//         transformationType: type,
+//         width: image?.width,
+//         height: image?.height,
+//         config: transformationConfig,
+//         secureURL: image?.secureURL,
+//         transformationURL: transformationUrl,
+//         aspectRatio: values.aspectRatio,
+//         prompt: values.prompt,
+//         color: values.color,
+//       }
+
+//       if (action === 'Add') {
+//         try {
+//           const newImage = await addImage({
+//             image: imageData,
+//             userId,
+//             path: '/'
+//           })
+
+//           if (newImage) {
+//             form.reset()
+//             setImage(data)
+//             router.push(`/transformations/${newImage._id}`)
+//           }
+//         } catch (error) {
+//           console.log(error);
+//         }
+//       }
+
+//       if (action === 'Update') {
+//         try {
+//           const updatedImage = await updateImage({
+//             image: {
+//               ...imageData,
+//               _id: data._id
+//             },
+//             userId,
+//             path: `/transformations/${data._id}`
+//           })
+
+//           if (updatedImage) {
+//             router.push(`/transformations/${updatedImage._id}`)
+//           }
+//         } catch (error) {
+//           console.log(error);
+//         }
+//       }
+//     }
+
+//     setIsSubmitting(false)
+//   }
+
+
+//   return (
+//     <div>
+//       TransformationForm
+//     </div>
+//   )
+// }
+
+// export default TransformationForm
